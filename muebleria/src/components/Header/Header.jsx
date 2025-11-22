157a7a2ed742b913585107767ef6204d4a7c9a1a
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2025-11-22 11:25:17 +0100

    fix header

--- muebleria/src/components/Header/Header.jsx
+++ muebleria/src/components/Header/Header.jsx
@@ -2,56 +2,74 @@ import { React, useState } from "react";
 import s from "./Header.module.css";
 
 const Header = () => {
-  const [isOpenMenu, setIsOpenMenu] = useState();
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
   return (
-    <div>
-      <nav class="hamburger-menu">
-        <div class={s.burgerMenu}>
-          <ul class={s.menuItems}>
-            <li>
-              <a href="#">Наші меблі</a>
-            </li>
-
-            <li>
-              <a href="#">Про нас</a>
-            </li>
-
-            <li>
-              <a href="#">Часті запитання</a>
-            </li>
-
-            <li>
-              <a href="#">Відгуки</a>
-            </li>
-          </ul>
-          <button>
-            <a href="#">До покупок</a>
-          </button>
+    <header>
+      <div className="container">
+        <div className={s.logo}>
+          <img src="#" alt="logo" />
         </div>
-        <div className={s.desktopMenu}>
-          <ul class={s.menuItems}>
-            <li>
-              <a href="#">Наші меблі</a>
-            </li>
-
-            <li>
-              <a href="#">Про нас</a>
-            </li>
-
-            <li>
-              <a href="#">Часті запитання</a>
-            </li>
-
-            <li>
-              <a href="#">Відгуки</a>
-            </li>
-          </ul>
-          <button>
-            <a href="#">До покупок</a>
+        <nav class="nav">
+          <button
+            onClick={() => {
+              setIsOpenMenu(true);
+            }}
+          >
+            change
           </button>
-        </div>
-      </nav>
-    </div>
+          {isOpenMenu && (
+            <div class={s.burgerMenu}>
+              <div className={s.logo}>
+                <img src="#" alt="logo" />
+              </div>
+              <button className={s.close}>x</button>
+              <ul class={s.menuItems}>
+                <li>
+                  <a href="#">Наші меблі</a>
+                </li>
+
+                <li>
+                  <a href="#">Про нас</a>
+                </li>
+
+                <li>
+                  <a href="#">Часті запитання</a>
+                </li>
+
+                <li>
+                  <a href="#">Відгуки</a>
+                </li>
+              </ul>
+              <button>
+                <a href="#">До покупок</a>
+              </button>
+            </div>
+          )}
+          <div className={s.desktopMenu}>
+            <ul class={s.menuItems}>
+              <li>
+                <a href="#">Наші меблі</a>
+              </li>
+
+              <li>
+                <a href="#">Про нас</a>
+              </li>
+
+              <li>
+                <a href="#">Часті запитання</a>
+              </li>
+
+              <li>
+                <a href="#">Відгуки</a>
+              </li>
+            </ul>
+            <button>
+              <a href="#">До покупок</a>
+            </button>
+          </div>
+        </nav>
+      </div>
+    </header>
   );
 };
 
