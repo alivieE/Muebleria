a9bb731f9f557de859ccfb1c2e91a6607588806b
alivieE <153372860+alivieE@users.noreply.github.com>
2026-03-25 12:49:19 +0200

    fix

--- muebleria/src/components/Feedbacks/Feedbacks.jsx
+++ muebleria/src/components/Feedbacks/Feedbacks.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import ReactStars from "react-stars";
+import { render } from "react-dom";
 import s from "./Feedbacks.module.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
@@ -10,9 +12,13 @@ import "swiper/css/scrollbar";
 const Feedbacks = () => {
   const [feedbacks, setFeedbacks] = useState([]);
 
+  const ratingChanged = (newRating) => {
+    console.log(newRating);
+  };
+
   useEffect(() => {
     fetch(
-      "https://furniture-store-v2.b.goit.study/api/feedbacks?limit=10&page=1"
+      "https://furniture-store-v2.b.goit.study/api/feedbacks?limit=10&page=1",
     )
       .then((res) => res.json())
       .then((data) => {
@@ -28,7 +34,8 @@ const Feedbacks = () => {
         <p className={s.info}>
           Дізнайтеся, що кажуть наші задоволені клієнти про нашу меблі.
         </p>
-        <Swiper className="userFeedbackSwiper"
+        <Swiper
+          className="userFeedbackSwiper"
           spaceBetween={50}
           slidesPerView={3}
           modules={[Navigation, Pagination]}
@@ -40,9 +47,15 @@ const Feedbacks = () => {
               <SwiperSlide className="userFeedback">
                 <li key={fb.id}>
                   <div className={s.feedback}>
-                    <p>{fb.rate}</p>
+                    <ReactStars
+                      count={5}
+                      value={fb.rate}
+                      size={24}
+                      edit={false}
+                      color1={"#d3d3d3"}
+                      color2={"#000000"}
+                    />
                     <p className={s.name}>{fb.name}</p>
-                    <p className={s.date}>{fb.date}</p>
                     <p className={s.descr}>{fb.descr}</p>
                   </div>
                 </li>
