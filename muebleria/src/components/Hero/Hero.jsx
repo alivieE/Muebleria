c7a3e6df750845823291f64bc572571a539bafbb
alivieE <153372860+alivieE@users.noreply.github.com>
2026-03-31 01:20:13 +0300

    hero desk

--- muebleria/src/components/Hero/Hero.jsx
+++ muebleria/src/components/Hero/Hero.jsx
@@ -3,20 +3,21 @@ import s from "./Hero.module.css";
 import images from "../../assets/index.js";
 
 const Hero = () => {
-  
   return (
     <div className={s.hero}>
       <div className="container">
         <div className={s.heroDescription}>
-          <p className={s.heroTitle}>Створіть свій ідеальний дім</p>
-          <p className={s.heroSubtitle}>
-            Відкрийте для себе колекцію стильних та комфортних меблів, які
-            перетворять ваш простір. Наша продукція поєднує в собі елегантність
-            та функціональність.
-          </p>
-          <button href="#" className={s.heroButton}>
-            Що про нас думають
-          </button>
+          <div className={s.heroDesktop}>
+            <p className={s.heroTitle}>Створіть свій ідеальний дім</p>
+            <p className={s.heroSubtitle}>
+              Відкрийте для себе колекцію стильних та комфортних меблів, які
+              перетворять ваш простір. Наша продукція поєднує в собі
+              елегантність та функціональність.
+            </p>
+            <button href="#" className={s.heroButton}>
+              Що про нас думають
+            </button>
+          </div>
         </div>
       </div>
     </div>
